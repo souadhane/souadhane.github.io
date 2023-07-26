92f7de922f4792e9df3114053a9a9a6925a41e78
Moahmed Amine Bellil <mbellil262@gmail.com>
2023-07-26 23:33:02 +0200

    mobile version added

---
+++ app.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector(".nav");
+const ul = document.querySelector(".ul_collapsed");
+
+nav.addEventListener("click", function () {
+  nav.classList.toggle("nav_expanded");
+  document;
+
+  ul.classList.toggle("ul_expanded");
+  console.log("hello");
+});
+console.log("hello");
