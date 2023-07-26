40cc7e2b2d6ab185eb750ad55bb30300f5b6e632
Moahmed Amine Bellil <mbellil262@gmail.com>
2023-07-26 23:56:46 +0200

    hero photo added

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const nav = document.querySelector(".nav");
 const ul = document.querySelector(".ul_collapsed");
+const icons = document.querySelectorAll(".icon");
+
+icons[0].addEventListener("click", function () {
+  window.open("https://www.instagram.com/elosa68/");
+});
+icons[1].addEventListener("click", function () {
+  window.open("https://www.tiktok.com/@elosa68");
+});
 
 nav.addEventListener("click", function () {
   nav.classList.toggle("nav_expanded");
